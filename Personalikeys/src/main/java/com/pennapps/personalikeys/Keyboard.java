967a946c3f5cd0f816bc1d12515a0980c09907b1
rigeljs <rigeljs@gmail.com>
2013-09-06 22:07:02 -0400

    fixed more keyboard errors

--- Personalikeys/src/main/java/com/pennapps/personalikeys/Keyboard.java
+++ Personalikeys/src/main/java/com/pennapps/personalikeys/Keyboard.java
@@ -1,11 +1,24 @@
 package com.pennapps.personalikeys;
 
+import android.view.MotionEvent;
 import android.view.View;
 import android.app.Activity;
 
 /**
  * Created by Nikki on 9/6/13.
  */
-public class Main extends Activity implements OnTouchListener, OnClickListener,
-        OnFocusChangeListener{
+public class Keyboard extends Activity implements View.OnTouchListener, View.OnClickListener,
+        View.OnFocusChangeListener{
+    public boolean onTouch(View v, MotionEvent m){
+        //TODO: implement this
+        return false;
+    }
+
+    public void onFocusChange(View v, boolean b){
+        //TODO: implement this
+    }
+
+    public void onClick(View v){
+        //TODO: implement this
+    }
 }
