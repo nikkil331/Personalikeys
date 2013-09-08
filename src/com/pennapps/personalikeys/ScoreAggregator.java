3181a6ea97f85d71a7a598612447610fa9a95279
Nicole Limtiaco <limni@seas.upenn.edu>
2013-09-08 02:15:37 -0400

    n-gram iterators

--- src/com/pennapps/personalikeys/ScoreAggregator.java
+++ src/com/pennapps/personalikeys/ScoreAggregator.java
@@ -1,13 +1,47 @@
 package com.pennapps.personalikeys;
 
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
+
 public class ScoreAggregator {
-	StringTokenizer tokenizer;
+	NgramIterator two_iter;
+	NgramIterator one_iter;
 	
 	public ScoreAggregator(String s){
-		tokenizer = new StringTokenizer(s);
+		one_iter = new NgramIterator(1, s);
+		two_iter = new NgramIterator(2, s);
 	}
-	
-	
+	private static class NgramIterator implements Iterator<String> {
+
+	    String[] words;
+	    int pos = 0, n;
+
+	    public NgramIterator(int n, String str) {
+	        this.n = n;
+	        words = str.split(" ");
+	    }
+
+	    public boolean hasNext() {
+	        return pos < words.length - n + 1;
+	    }
+
+	    public String next() {
+	        StringBuilder sb = new StringBuilder();
+	        for (int i = pos; i < pos + n; i++)
+	            sb.append((i > pos ? " " : "") + words[i]);
+	        pos++;
+	        return sb.toString();
+	    }
+
+	    public void remove() {
+	        throw new UnsupportedOperationException();
+	    }
+	}
+	public static void main(String[] args){
+		ScoreAggregator sa = new ScoreAggregator("This is a sentence");
+		while(sa.two_iter.hasNext()){
+			System.out.println(sa.two_iter.next());
+		}
+	}
 }
